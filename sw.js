c45be85d89044045540425533a78eacd5307f2e2
FileyOS <ghorn5812@gmail.com>
2026-04-28 09:24:24 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,12 @@
+importScripts("/scram/scramjet.all.js");
+
+const { ScramjetServiceWorker } = $scramjetLoadWorker();
+const sw = new ScramjetServiceWorker();
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith((async () => {
+    await sw.loadConfig();
+    if (sw.route(event)) return sw.fetch(event);
+    return fetch(event.request);
+  })());
+});
